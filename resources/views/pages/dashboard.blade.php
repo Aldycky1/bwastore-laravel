028030c03c9694484716b6d01d1d3b074fd344bc
Aldycky1 <bagusaldycky@gmail.com>
2021-08-12 20:04:45 +0700

    integrated dashboard.blade.php

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -30,7 +30,7 @@
                         <div
                             class="dashboard-card-subtitle"
                         >
-                            15,209
+                            {{ number_format($customer) }}
                         </div>
                     </div>
                 </div>
@@ -46,7 +46,7 @@
                         <div
                             class="dashboard-card-subtitle"
                         >
-                            $931,290
+                            ${{ number_format($revenue) }}
                         </div>
                     </div>
                 </div>
@@ -62,7 +62,7 @@
                         <div
                             class="dashboard-card-subtitle"
                         >
-                            22,409,399
+                            {{ number_format($transaction_count) }}
                         </div>
                     </div>
                 </div>
@@ -73,102 +73,40 @@
                 <h5 class="mb-3">
                     Recent Transactions
                 </h5>
-                <a
-                    href="/dashboard-transactions-details.html"
-                    class="card card-list d-block"
-                >
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-1">
-                                <img
-                                    src="/images/dashboard-icon-product-1.png"
-                                    alt=""
-                                />
-                            </div>
-                            <div class="col-md-4">
-                                Shirup Marzzan
-                            </div>
-                            <div class="col-md-3">
-                                Angga Risky
-                            </div>
-                            <div class="col-md-3">
-                                12 Januari, 2020
-                            </div>
-                            <div
-                                class="col-md-1 d-none d-md-block"
-                            >
-                                <img
-                                    src="/images/dashboard-arrow-right.svg"
-                                    alt=""
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <a
-                    href="/dashboard-transactions-details.html"
-                    class="card card-list d-block"
-                >
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-1">
-                                <img
-                                    src="/images/dashboard-icon-product-2.png"
-                                    alt=""
-                                />
-                            </div>
-                            <div class="col-md-4">
-                                LeBrone X
-                            </div>
-                            <div class="col-md-3">
-                                Masayoshi
-                            </div>
-                            <div class="col-md-3">
-                                11 January, 2020
-                            </div>
-                            <div
-                                class="col-md-1 d-none d-md-block"
-                            >
-                                <img
-                                    src="/images/dashboard-arrow-right.svg"
-                                    alt=""
-                                />
+                @foreach ($transaction_data as $transaction)
+                    <a
+                        href="{{ route('dashboard-transaction-details', $transaction->id) }}"
+                        class="card card-list d-block"
+                    >
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-1">
+                                    <img
+                                        src="{{ Storage::url($transaction->product->productGalleries->first()->photos ?? '') }}"
+                                        class="w-75"
+                                    />
+                                </div>
+                                <div class="col-md-4">
+                                    {{ $transaction->product->name ?? '' }}
+                                </div>
+                                <div class="col-md-3">
+                                    {{ $transaction->transaction->user->name ?? '' }}
+                                </div>
+                                <div class="col-md-3">
+                                    {{ $transaction->created_at ?? '' }}
+                                </div>
+                                <div
+                                    class="col-md-1 d-none d-md-block"
+                                >
+                                    <img
+                                        src="/images/dashboard-arrow-right.svg"
+                                        alt=""
+                                    />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-                <a
-                    href="/dashboard-transactions-details.html"
-                    class="card card-list d-block"
-                >
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-1">
-                                <img
-                                    src="/images/dashboard-icon-product-1.png"
-                                    alt=""
-                                />
-                            </div>
-                            <div class="col-md-4">
-                                Soffa Lembutte
-                            </div>
-                            <div class="col-md-3">
-                                Shayna
-                            </div>
-                            <div class="col-md-3">
-                                11 January, 2020
-                            </div>
-                            <div
-                                class="col-md-1 d-none d-md-block"
-                            >
-                                <img
-                                    src="/images/dashboard-arrow-right.svg"
-                                    alt=""
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </a>
+                    </a>                    
+                @endforeach
             </div>
         </div>
     </div>
