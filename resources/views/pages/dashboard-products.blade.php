e32d1408c793450f4046121562d451c7fdc066a8
Aldycky1 <bagusaldycky@gmail.com>
2021-08-12 22:39:57 +0700

    updated dashboard-products

--- resources/views/pages/dashboard-products.blade.php
+++ resources/views/pages/dashboard-products.blade.php
@@ -21,7 +21,7 @@
             <div class="row">
                 <div class="col-12">
                     <a
-                        href="/dashboard-products-create.html"
+                        href="{{ route('dashboard-product-create') }}"
                         class="btn btn-success"
                     >
                         Add New Products
@@ -29,116 +29,30 @@
                 </div>
             </div>
             <div class="row mt-4">
-                <div
-                    class="col-12 col-sm-6 col-md-4 col-lg-3"
-                >
-                    <a
-                        href="/dashboard-products-details.html"
-                        class="card card-dashboard-product d-block"
+                @foreach ($products as $product)
+                    <div
+                        class="col-12 col-sm-6 col-md-4 col-lg-3"
                     >
-                        <div class="card-body">
-                            <img
-                                src="/images/product-card-1.png"
-                                alt=""
-                                class="w-100 mb-2"
-                            />
-                            <div class="product-title">
-                                Shirup Marzzan
+                        <a
+                            href="{{ route('dashboard-product-details', $product->id) }}"
+                            class="card card-dashboard-product d-block"
+                        >
+                            <div class="card-body">
+                                <img
+                                    src="{{ Storage::url($product->productGalleries->first()->photos ?? '') }}"
+                                    alt=""
+                                    class="w-100 mb-2"
+                                />
+                                <div class="product-title">
+                                    {{ $product->name }}
+                                </div>
+                                <div class="product-category">
+                                    {{ $product->category->name }}
+                                </div>
                             </div>
-                            <div class="product-category">
-                                Foods
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div
-                    class="col-12 col-sm-6 col-md-4 col-lg-3"
-                >
-                    <a
-                        href="/dashboard-products-details.html"
-                        class="card card-dashboard-product d-block"
-                    >
-                        <div class="card-body">
-                            <img
-                                src="/images/product-card-2.png"
-                                alt=""
-                                class="w-100 mb-2"
-                            />
-                            <div class="product-title">
-                                Shirup Marzzan
-                            </div>
-                            <div class="product-category">
-                                Foods
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div
-                    class="col-12 col-sm-6 col-md-4 col-lg-3"
-                >
-                    <a
-                        href="/dashboard-products-details.html"
-                        class="card card-dashboard-product d-block"
-                    >
-                        <div class="card-body">
-                            <img
-                                src="/images/product-card-3.png"
-                                alt=""
-                                class="w-100 mb-2"
-                            />
-                            <div class="product-title">
-                                Shirup Marzzan
-                            </div>
-                            <div class="product-category">
-                                Foods
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div
-                    class="col-12 col-sm-6 col-md-4 col-lg-3"
-                >
-                    <a
-                        href="/dashboard-products-details.html"
-                        class="card card-dashboard-product d-block"
-                    >
-                        <div class="card-body">
-                            <img
-                                src="/images/product-card-4.png"
-                                alt=""
-                                class="w-100 mb-2"
-                            />
-                            <div class="product-title">
-                                Shirup Marzzan
-                            </div>
-                            <div class="product-category">
-                                Foods
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div
-                    class="col-12 col-sm-6 col-md-4 col-lg-3"
-                >
-                    <a
-                        href="/dashboard-products-details.html"
-                        class="card card-dashboard-product d-block"
-                    >
-                        <div class="card-body">
-                            <img
-                                src="/images/product-card-5.png"
-                                alt=""
-                                class="w-100 mb-2"
-                            />
-                            <div class="product-title">
-                                Shirup Marzzan
-                            </div>
-                            <div class="product-category">
-                                Foods
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                        </a>
+                    </div>                    
+                @endforeach
             </div>
         </div>
     </div>
